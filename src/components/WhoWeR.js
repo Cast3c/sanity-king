70576f8c7706af77b1c6e552347bfce50f31de44
Cast3c <ricardo.castellarp@gmail.com>
2022-11-28 23:20:10 -0500

    testing changes

--- src/components/WhoWeR.js
+++ src/components/WhoWeR.js
@@ -5,18 +5,30 @@ export const WhoWeR = () => {
   return (
     <div className="contenido" id="Quienes-somos">
       <div className="cuadro-contenido">
-        <div className="leftAboutUs"></div>
-        <div className="rightAboutUs">
-        </div>
         <h1 className="aboutUsSanity">SANITY</h1>
         <h1 className="aboutUsKing">KING</h1>
-        <h3 className="aboutUsTitle">Lorem Ipsum...</h3>
-        <p className="aboutUsParaf">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a lacus ex. Suspendisse gravida, eros non tempus porttitor, metus neque rhoncus velit, sit amet accumsan enim odio in ex. Nullam aliquam sapien quis eleifend vestibulum. Quisque et ornare neque. Sed quis sapien sit amet lorem blandit accumsan. Donec mollis tellus ligula, sagittis tempor eros scelerisque vitae. Sed laoreet mauris efficitur mi finibus, a porta turpis rutrum. Fusce suscipit non velit eu elementum. Fusce vestibulum lorem vehicula nisi dignissim, et fermentum eros semper. Donec ac risus eget quam imperdiet semper.</p>
-        <span className="logoCarousel">
-          <h1 className="sLogo">S</h1>
-          <h1 className="kLogo">K</h1>
-          <h3 className="logoWord">Logo</h3>
-        </span>
+        <div className="leftAboutUs">
+          <h3 className="aboutUsTitle">Lorem Ipsum...</h3>
+          <p className="aboutUsParaf">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque a lacus ex. Suspendisse gravida, eros non tempus
+            porttitor, metus neque rhoncus velit, sit amet accumsan enim odio in
+            ex. Nullam aliquam sapien quis eleifend vestibulum. Quisque et
+            ornare neque. Sed quis sapien sit amet lorem blandit accumsan. Donec
+            mollis tellus ligula, sagittis tempor eros scelerisque vitae. Sed
+            laoreet mauris efficitur mi finibus, a porta turpis rutrum. Fusce
+            suscipit non velit eu elementum. Fusce vestibulum lorem vehicula
+            nisi dignissim, et fermentum eros semper. Donec ac risus eget quam
+            imperdiet semper.
+          </p>
+        </div>
+        <div className="rightAboutUs">
+          <span className="logoCarousel">
+            <h1 className="sLogo">S</h1>
+            <h1 className="kLogo">K</h1>
+            <h3 className="logoWord">Logo</h3>
+          </span>
+        </div>
       </div>
     </div>
   );
